68875bae740be93cd8b9dc59bfe3b92429c12948
Diegolivia <diegolivia@hotmail.com>
2021-10-08 11:50:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from transformers import  AutoModelForQuestionAnswering , AutoTokenizer, pipeline
 from flask import Flask, jsonify, request
 from flask_cors import cross_origin,CORS
+name_model = "francoMG/sara-qa"
+tokenizer_model = "dccuchile/bert-base-spanish-wwm-uncased"
 #model = pickle.load(open('modelo-qa','rb'))
-name_model = 'amoux/scibert_nli_squad'
 #name_model = 'ktrapeznikov/scibert_scivocab_uncased_squad_v2'
+#name_model = 'mrm8488/distill-bert-base-spanish-wwm-cased-finetuned-spa-squad2-es'
+#tokenizer_model = "mrm8488/distill-bert-base-spanish-wwm-cased-finetuned-spa-squad2-es"
 #name_model = 'amoux/scibert_nli_squad'
 model = AutoModelForQuestionAnswering.from_pretrained(name_model)
 tokenizer= AutoTokenizer.from_pretrained(name_model,do_lower_case=False)
@@ -26,13 +29,20 @@ def preguntar():
 	resp=""
 	for dat in _contexto:
 		try:
-			payload = nlp(question= _pregunta, context= dat,max_length=512)
-			if payload['score']>score:
-				score=payload['score']
-				resp=payload['answer']
-		except:
-			print("An NLP Tokenization Exception has happened")
-	return jsonify({'Respuesta':resp})
+			payload = nlp(question=_pregunta,context=dat)
+			val=payload['score']
+			print(payload)
+		except Exception as e:
+			print(e)
+			print("An Exception has happened")
+		if val>score:
+			score=val
+			resp=payload['answer']
+			print("===================================")
+			print(dat)
+			print(payload)
+			print("===================================")
+	return jsonify({'Respuesta':resp,'Score':score})
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0",port=5000,debug=False)
