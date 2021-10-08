63d6052d2f11cc346a0840da4eec075ee9961435
Diegolivia <diegolivia@hotmail.com>
2021-10-08 01:19:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,37 @@
-from transformers import  AutoModelForQuestionAnswering , AutoTokenizer, pipeline
+from transformers import QuestionAnsweringPipeline,BertForQuestionAnswering,BertTokenizerFast
 from flask import Flask, jsonify, request
 from flask_cors import cross_origin,CORS
 #model = pickle.load(open('modelo-qa','rb'))
-name_model = 'amoux/scibert_nli_squad'
+name_model = "francoMG/sara-qa"
+tokenizer_model = "dccuchile/bert-base-spanish-wwm-uncased"
 #name_model = 'ktrapeznikov/scibert_scivocab_uncased_squad_v2'
 #name_model = 'amoux/scibert_nli_squad'
-model = AutoModelForQuestionAnswering.from_pretrained(name_model)
-tokenizer= AutoTokenizer.from_pretrained(name_model,do_lower_case=False)
-nlp = pipeline('question-answering', model = model, tokenizer = tokenizer,framework="pt")
+model = BertForQuestionAnswering.from_pretrained(name_model)
+tokenizer= BertTokenizerFast.from_pretrained(tokenizer_model,do_lower_case=False)
+nlp = QuestionAnsweringPipeline(model,tokenizer,framework="pt")
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 @cross_origin(origin='*')
 def ServerStatus():
-	return "Server Iniciado"
+	return "Server Started"
 
 @app.route('/preguntar', methods=['POST'])
 @cross_origin(origin='*')
 def preguntar():
-	_contexto = request.json['contexto']
 	_pregunta = request.json['pregunta']
 	score=-1
 	resp=""
-	for dat in _contexto:
+	for dat in request.json['contexto']:
 		try:
-			payload = nlp(question= _pregunta, context= dat,max_length=512)
+			payload = nlp(question=_pregunta,context=dat)
 			if payload['score']>score:
 				score=payload['score']
 				resp=payload['answer']
-		except:
-			print("An NLP Tokenization Exception has happened")
+		except Exception as e:
+			print(e)
+			print("An Exception has happened")
 	return jsonify({'Respuesta':resp,'Score':score})
 
 if __name__ == '__main__':
